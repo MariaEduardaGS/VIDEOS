b2e3adaa1263855a9e27c2e3a0fa89be5d2fa275
MariaEduardaGS <30415543+MariaEduardaGS@users.noreply.github.com>
2017-11-12 18:55:13 -0200

    Create questao4.js

---
+++ questao4.js
@@ -0,0 +1,11 @@
+var A=[], B=[], C=[];
+    for(i=0;i<10;i++){
+    A[i]=parseInt(prompt("Escreva um numero"));
+}  
+    for(i=0;i<10;i++){
+     B[i]=parseInt(prompt("Escreva um numero"));
+}
+    for(i=0;i<10;i++){
+    C[i]=A[i]+B[i];
+    alert(C[i]+ " ");
+}
